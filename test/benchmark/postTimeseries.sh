4746aaaf662bf31aa1cff7791d32d10aff288950
shatrughan <100255795+rai-shatrughan@users.noreply.github.com>
2022-06-26 11:42:33 +0530

    10k req/sec

--- test/benchmark/postTimeseries.sh
+++ test/benchmark/postTimeseries.sh
@@ -2,11 +2,11 @@
 start=`date`
 echo "Start Time - " $start
 
-go-wrk -c 2000 -d 10 -T 30000 -M PUT \
+go-wrk -c 8000 -d 10 -T 30000 -M PUT \
 -H "X-API-Key: srkey12345" \
 -H "Content-Type: application/json" \
 -body @json/timeseries.json \
-http://localhost:8080/api/v1/timeseries/6fdae6af-226d-48bd-8b61-699758137eb3
+http://localhost:8002/api/v1/timeseries/6fdae6af-226d-48bd-8b61-699758137eb3
 
 end=`date`
 echo "End Time - " $end
